5d86a7a9d5757440078ff2f94985cb21797de9d8
bxxstian <seb_faull@hotmail.co.uk>
2018-11-11 16:23:41 +0000

    Creating a new class and Package

--- src/com/teamtreehouse/Main.java
+++ src/com/teamtreehouse/Main.java
@@ -1,9 +1,14 @@
 package com.teamtreehouse;
 
+import com.teamtreehouse.data.Person;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Person person = new Person("John Smith");
+        String name = person.getName();
+
+        // Type 'sout' as shortcut
+        System.out.println(name);
     }
 }
-
---
+++ src/com/teamtreehouse/data/Person.java
@@ -0,0 +1,15 @@
+package com.teamtreehouse.data;
+
+public class Person {
+    // Declare a name field
+    private String name;
+
+    // Initialise by adding a constructor
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
